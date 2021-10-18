d47850e84af93bec3d9f17f8b626fa9cfe326c85
Maria Bikarska <92679864+MimBikarska88@users.noreply.github.com>
2021-10-18 09:48:33 +0300

    Add files via upload

---
+++ ListyIterator/ListyIterator/ListyIterator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> _list;
+        private int index;
+
+        public ListyIterator(params T[]collection)
+        {
+            _list = new List<T>(collection);
+            index = 0;
+        }
+
+        public bool Move()
+        {
+            if (this.HasNext())
+            {
+                index++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool HasNext() => index < _list.Count - 1;
+
+        public void Print()
+        {
+            try
+            {
+                Console.WriteLine(_list[index]);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                throw new InvalidOperationException("Invalid operation!");
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var i in _list)
+            {
+                yield return i;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ ListyIterator/ListyIterator/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> listyIterator = null;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }else if (input.Contains("Create"))
+                {
+                    var collection = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    if (collection.Length > 1)
+                    {
+                        listyIterator = new ListyIterator<string>(collection.Skip(1).ToArray());
+
+                    }
+                    else
+                    {
+                        listyIterator = new ListyIterator<string>();
+                    }
+                }else if (input.Contains("Move"))
+                {
+                    Console.WriteLine(listyIterator.Move());
+                }else if (input.Contains("HasNext"))
+                {
+                    Console.WriteLine(listyIterator.HasNext());
+                }else if (input.Contains("PrintAll"))
+                {
+                    Console.WriteLine(string.Join(" ",listyIterator));
+                }else if (input.Contains("Print"))
+                {
+                    try
+                    {
+                        listyIterator.Print();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Invalid operation!");
+                    }
+                }
+                
+            }
+        }
+    }
+}
